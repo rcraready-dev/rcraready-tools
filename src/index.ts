35a72a1d61c2c039416940ae0721a8982a0c8333
rcraready-dev <dev@rcraready.com>
2026-05-17 12:29:14 -0600

    Export focused RCRA helpers

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 export {
+  GENERATOR_CATEGORY_PROFILES,
+  KG_PER_LB,
   classifyGeneratorStatus,
+  getGeneratorCategoryProfile,
+  kilogramsToPounds,
+  poundsToKilograms,
   type GeneratorCategory,
+  type GeneratorCategoryProfile,
   type GeneratorStatusInput,
   type GeneratorStatusResult,
 } from './generator-status';
 
 export {
   calculateAccumulationDeadline,
+  calculateSatelliteAccumulationDeadline,
   calculateSatelliteMoveDeadline,
   type AccumulationDeadlineInput,
   type AccumulationDeadlineResult,
   type DateLike,
+  type SatelliteAccumulationDeadlineInput,
+  type SatelliteAccumulationDeadlineResult,
   type SatelliteMoveDeadlineInput,
   type SatelliteMoveDeadlineResult,
 } from './accumulation-deadline';
