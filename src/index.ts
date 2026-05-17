1545c771dddd35c299ac21340f2736e701aa91fa
rcraready-dev <dev@rcraready.com>
2026-05-17 12:24:58 -0600

    Export public helpers

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export {
+  classifyGeneratorStatus,
+  type GeneratorCategory,
+  type GeneratorStatusInput,
+  type GeneratorStatusResult,
+} from './generator-status';
+
+export {
+  calculateAccumulationDeadline,
+  calculateSatelliteMoveDeadline,
+  type AccumulationDeadlineInput,
+  type AccumulationDeadlineResult,
+  type DateLike,
+  type SatelliteMoveDeadlineInput,
+  type SatelliteMoveDeadlineResult,
+} from './accumulation-deadline';
